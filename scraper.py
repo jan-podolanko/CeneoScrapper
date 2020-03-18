1e70eed7f40573cc56e2cbf2fedf9255332e450e
jan-podolanko <jan.podolanko11@gmail.com>
2020-03-18 14:45:06 +0100

    JP 2020-03-18

--- scraper.py
+++ scraper.py
@@ -15,15 +15,31 @@ page_tree = BeautifulSoup(page_response.text, 'html.parser')
 opinions = page_tree.find_all('li', 'review-box')
 
 #5 wydobycie skladowych dla pojedynczej opinii
-opinion = opinions.pop()
-
-opinion_id = opinion["data-entry-id"]
-author = opinion.find('div','reviewer-name-line').string
-recommendation = opinion.find('div','product-review-summary').find('em').string
-stars = opinion.find('span', 'review-score-count').string
-purchased = opinion.find('div','product-review-pz').string
-useful = opinion.find('button','vote-yes').find('span').string
-useless = opinion.find('button','vote-no').find('span').string
-content = opinion.find('p','product-review-body').get_text()
-
-#dokoncz daty i pobieraj wszystkie opinie ()
+for opinion in opinions:
+    opinion_id = opinion["data-entry-id"]
+    author = opinion.find('div','reviewer-name-line').string
+    recommendation = opinion.find('div','product-review-summary').find('em').string
+    stars = opinion.find('span', 'review-score-count').string
+    try:
+        purchased = opinion.find('div','product-review-pz').string
+    except AttributeError:
+        purchased = None
+    useful = opinion.find('button','vote-yes').find('span').string
+    useless = opinion.find('button','vote-no').find('span').string
+    content = opinion.find('p','product-review-body').get_text()
+    dates = opinion.find('span','review-time').find_all('time')
+    review_date = dates.pop(0)['datetime']
+    try:
+        purchase_date = dates.pop(0)['datetime']
+    except AttributeError:
+        purchase_date = None
+    try:
+        pros = opinion.find('div','pros-cell').find('ul').get_text()
+    except AttributeError:
+        pros = None
+    try:
+        cons = opinion.find('div','cons-cell').find('ul').get_text()
+    except AttributeError:
+        cons = None
+        
+#zrobic to do wszystkich opinii
