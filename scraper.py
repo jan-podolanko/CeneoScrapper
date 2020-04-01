f898ebb422fd70ff72be7c37c7dc73811590ad23
jan-podolanko <jan.podolanko11@gmail.com>
2020-04-01 15:13:31 +0200

    JP 2020-04-01

--- scraper.py
+++ scraper.py
@@ -3,6 +3,30 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 import json
+#dodanie funkcji do ekstrakcji
+# 'div', 'product-review-summary' 'em'
+def extract_feature(opinion, tag, tag_class, child=None):
+    try:
+        if child:
+            return opinion.find(tag, tag_class).find(child).get_text().strip()
+        else:
+            return opinion.find(tag, tag_class).get_text().strip()
+    except AttributeError:
+        return None
+
+tags = {
+    "recommendation":["div","product-review-summary", "em"],
+    "stars":["span", "review-score-count"],
+    "content":["p","product-review-body"],
+    "author":["div", "reviewer-name-line"],
+    "pros":["div", "pros-cell", "ul"],
+    "cons":["div", "cons-cell", "ul"], 
+    "useful":["button","vote-yes", "span"],
+    "useless":["button","vote-no", "span"],
+    "purchased":["div", "product-review-pz", "em"]
+}
+
+
 #2 zdefiniowanie adresu url przykladowej strony z opiniami
 
 url_prefix = 'https://www.ceneo.pl'
@@ -24,53 +48,19 @@ while url:
 
     #5 wydobycie skladowych dla pojedynczej opinii
     for opinion in opinions:
-        opinion_id = opinion["data-entry-id"]
-        author = opinion.find('div','reviewer-name-line').string
+        features = {key:extract_feature(opinion, *args)
+                    for key, args in tags.items()}
+        features["purchased"] = (features["purchased"]=="Opinia potwierdzona zakupem")
+        features["opinion_id"] = opinion["data-entry-id"]
+        dates = opinion.find("span", "review-time").find_all("time")
+        features["review_date"] = dates.pop(0)["datetime"]
         try:
-            recommendation = opinion.find('div','product-review-summary').find('em').string
-        except AttributeError:
-            recommendation = None
-        stars = opinion.find('span', 'review-score-count').string
-        try:
-            purchased = opinion.find('div','product-review-pz').string
-        except AttributeError:
-            purchased = None
-        useful = opinion.find('button','vote-yes').find('span').string
-        useless = opinion.find('button','vote-no').find('span').string
-        content = opinion.find('p','product-review-body').get_text()
-        dates = opinion.find('span','review-time').find_all('time')
-        review_date = dates.pop(0)['datetime']
-        try:
-            purchase_date = dates.pop(0)['datetime']
+            features["purchase_date"] = dates.pop(0)["datetime"]
         except IndexError:
-            purchase_date = None
-        try:
-            pros = opinion.find('div','pros-cell').find('ul').get_text()
-        except AttributeError:
-            pros = None
-        try:
-            cons = opinion.find('div','cons-cell').find('ul').get_text()
-        except AttributeError:
-            cons = None
+            features["purchase_date"] = None
 
-        opinion_dict = {
-            "opinion_id":opinion_id,
-            "recommendation":recommendation,
-            "stars":stars,
-            "content":content,
-            "author":author,
-            "pros":pros,
-            "cons":cons,
-            "useful":useful,
-            "useless":useless,
-            "purchased":purchased,
-            "purchase_date":purchase_date,
-            "review_date":review_date
-        }
+        opinions_list.append(features)
 
-        opinions_list.append(opinion_dict)
-
-    #pprint.pprint(opinions_list)
     try:
         url = url_prefix+page_tree.find('a','pagination__next')['href']
     except TypeError:
@@ -78,6 +68,7 @@ while url:
     print(url)
 
 with open(product_id+'.json','w',encoding='utf-8') as fp:
-    json.dump(opinions_list, fp, ensure_ascii = False)
+    json.dump(opinions_list, fp, ensure_ascii = False, indent=4, separators=(',',': '))
 
 print(len(opinions_list))
+#pprint.pprint(opinions_list)
